928299ff52409138dd8685bde9fc86fbd13b3fc1
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-04-04 11:42:31 +0200

    feat(user): add authentication feature

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,5 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
+
+require('bootstrap');
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,53 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct(
+        private UserPasswordHasherInterface $hasher,
+    ) {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $user = (new User)
+            ->setFirstName('Pierre')
+            ->setLastName('Bertrand')
+            ->setEmail('admin@test.com')
+            ->setPassword(
+                $this->hasher->hashPassword(new User, 'Test1234!'),
+            )
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = (new User)
+                ->setFirstName($this->faker->firstName())
+                ->setLastName($this->faker->lastName())
+                ->setEmail($this->faker->unique()->email())
+                ->setPassword(
+                    $this->hasher->hashPassword(new User, 'Test1234!'),
+                )
+                ->setRoles(
+                    $this->faker->randomElements(['ROLE_USER', 'ROLE_ADMIN', 'ROLE_EDITOR'])
+                );
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
